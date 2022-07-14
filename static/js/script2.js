886d75f3bcba56b1f051d2491e39ebd9a3934796
Eddie Ward <eddie.musashi1@gmail.com>
2022-07-14 08:53:06 -0400

    3

--- static/js/script2.js
+++ static/js/script2.js
@@ -4,10 +4,14 @@ let timeline = new TimelineMax();
 timeline
   .to(".rock", 10, {x:-300})
   .to(".girl", 10, {y:-350}, "-=10")
+  .fromTo(".main-title", { y: -75 }, { y: 0, duration: 10 }, "-=10")
   .fromTo(".bg1", { y: -50 }, { y: 0, duration: 10 }, "-=10")
   .to(".content", 10, { top: "0%" }, "-=10")
-  .fromTo(".content-images", { opacity: 0 }, { opacity: 1, duration: 3 })
-  .fromTo(".text", { opacity: 0 }, { opacity: 1, duration: 3 });
+
+  .fromTo(".bio-text", { opacity: 0 }, { opacity: 1, duration: 6 })  
+
+  .fromTo(".content-images", { opacity: 0 }, { opacity: 1, duration: 6 })
+  .fromTo(".text", { opacity: 0 }, { opacity: 1, duration: 6 });
 
 let scene = new ScrollMagic.Scene({
   triggerElement: "section",
