8429c8a01a0b006ad8e9de6f80c63e7cf27a26d5
Eddie Ward <eddie.musashi1@gmail.com>
2022-07-18 11:28:42 -0400

    fade ins complete. need to just do hover animations and loading animations

--- static/js/script.js
+++ static/js/script.js
@@ -23,6 +23,7 @@ timeline
 
 .fromTo('.from-left-3', { opacity: 0, x:-300 }, { opacity: 1, x:0, duration: 2 })  
 
+
 let scene = new ScrollMagic.Scene({
   triggerElement: ".main",
   duration: "350%",
@@ -31,4 +32,43 @@ let scene = new ScrollMagic.Scene({
   .setTween(timeline)
   .addTo(controller);
 
+let timeline2 = new TimelineMax();
+
+timeline2
+.fromTo('.about-big-text', {x: -300, opacity : 0}, {x:0, opacity: 1, duration: 6 })  
+.fromTo('.fade1', {x: 300, opacity : 0}, {x:0, opacity: 1, duration: 6 })  
+.fromTo('.fade2', {x: -300, opacity : 0}, {x:0, opacity: 1, duration: 6 })  
+.fromTo('.fade3', {x: 300, opacity : 0}, {x:0, opacity: 1, duration: 6 })  
+.fromTo('.fade4', {x: -300, opacity : 0}, {x:0, opacity: 1, duration: 6 })  
+.fromTo('.fade5', {x: 300, opacity : 0}, {x:0, opacity: 1, duration: 6})  
+.fromTo('.fade6', {x: -300, opacity : 0}, {x:0, opacity: 1, duration: 6})  
+.fromTo('.fade7', {x: 300, opacity : 0}, {x:0, opacity: 1, duration: 6 })  
+
+
+let scene2 = new ScrollMagic.Scene({
+  triggerElement: ".about",
+  duration: "120%",
+  triggerHook: "100",
+})
+  .setTween(timeline2)
+  .addTo(controller);
+
+
+
+let timeline3 = new TimelineMax();
+
+timeline3
+.fromTo('.map-fade1', {x: -300, opacity : 0}, {x:0, opacity: 1, duration: 6 })  
+.fromTo('.email-btn', {x: 300, opacity : 0}, {x:0, opacity: 1, duration: 6 })  
+
+
+
+
+let scene3 = new ScrollMagic.Scene({
+  triggerElement: ".contact",
+  duration: "100%",
+  triggerHook: "100",
+})
+  .setTween(timeline3)
+  .addTo(controller);
 
